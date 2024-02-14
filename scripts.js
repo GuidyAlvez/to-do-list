dc1817176e3490dc8dab7dcd1d49f9588ad9801a
GuidyAlvez <143773462+GuidyAlvez@users.noreply.github.com>
2024-02-14 23:53:33 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,81 @@
+const button = document.querySelector(".button-add-task");
+const input = document.querySelector(".input-task");
+const listaCompleta = document.querySelector(".list-tasks");
+
+let minhaListaDeItens = [];
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  });
+
+  input.value = "";
+
+  mostrarTarefas();
+}
+
+function mostrarTarefas() {
+  let novaLi = "";
+
+  // ['comprar café', 'estudar programação']
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi =
+      novaLi +
+      `
+
+        <li class="task ${item.concluida && "done"}">
+            <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+            <p>${item.tarefa}</p>
+            <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+        </li>
+        
+        `;
+  });
+
+  listaCompleta.innerHTML = novaLi;
+
+  localStorage.setItem("lista", JSON.stringify(minhaListaDeItens));
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida;
+
+  mostrarTarefas();
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1);
+
+  mostrarTarefas();
+}
+
+function recarregarTarefas() {
+  const tarefasDoLocalStorage = localStorage.getItem("lista");
+
+  if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage);
+  }
+
+  mostrarTarefas();
+}
+
+recarregarTarefas();
+button.addEventListener("click", adicionarNovaTarefa);
+
+const getHours = () => {
+    const clock = document.getElementsByClassName('clock')[0]
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const hour = hours < 10 ? `0${hours}` : hours
+    const minute = minutes < 10 ? `0${minutes}` : minutes
+    const second = seconds < 10 ? `0${seconds}` : seconds
+    clock.innerHTML = `${hour}:${minute}:${second}`
+  }
+  
+  setInterval(() => {
+    getHours()
+  }, 1000)
